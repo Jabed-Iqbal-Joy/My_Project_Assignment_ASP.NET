7a427107b0758aadbd96ebb8f3223ef18b0642b7
Jabed Iqbal Joy <jjabediqbaljoy@gmail.com>
2024-03-03 02:46:33 +0600

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace My_Project_Assignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
